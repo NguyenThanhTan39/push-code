c44fc1198102f01a1a60dc5a3aa9126c34527822
NguyenThanhTan39 <ntt074204003354@gmail.com>
2024-09-06 07:29:06 +0700

    programming

---
+++ global_variables/global_variables.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int g;
+
+int main()
+{
+    int a, b;
+
+    a = 10;
+
+    b = 20;
+
+    g = a + b;
+
+    printf("value of a: %d, value of b: %d, value of g: %d",a,b,g);
+
+    return 0;
+}
