7159b92204eac81377330b3cda1a33a6e2602b18
NguyenThanhTan39 <153705085+NguyenThanhTan39@users.noreply.github.com>
2026-08-17 12:33:47 +0700

    Delete Game GoDot/first_game directory

--- Game GoDot/first_game/Scripts/game_manager.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var score = 0
-
-@onready var score_labels: Label = $ScoreLabels
-
-func add_point() -> void:
-	score += 1
-	score_labels.text = "You collected " + str(score) + " coins."
